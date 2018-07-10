dbe9306027ea96be60f9f225a9565f1ec7a7aca9
Camila Freitas <camila-nf@segplan.go.gov.br>
2018-07-10 14:22:48 -0300

    Aula 10/07

--- src/main/java/aula/camila/Objeto.java
+++ src/main/java/aula/camila/Objeto.java
@@ -2,8 +2,25 @@ package aula.camila;
 
 public class Objeto {
 
-    public void cumprimentar(){
-        System.out.println("Olá eu sou o Objeto , você quer ser meu amigo?");
+    private String nome;
+
+    public Objeto(){
+
     }
 
+    public Objeto(String nome){
+        this.nome = nome;
+    }
+
+    public void cumprimentar(){
+        System.out.println("Olá eu sou o " + nome +  " , você quer ser meu amigo?");
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
--- src/main/java/aula/camila/Principal.java
+++ src/main/java/aula/camila/Principal.java
@@ -7,8 +7,7 @@ public class Principal {
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
-        Objeto o = (Objeto) context.getBean("camila");
-        o.cumprimentar();
+        Tela tela = (Tela) context.getBean("tela");
     }
 
 }
